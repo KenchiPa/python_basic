25ecf24d7ca516360a283495a22214a5849d12b3
KenchiPa <kenchi6734@naver.com>
2023-02-04 14:50:33 +0900

    함수를 사용한 경우

--- myvenv/Chapter06/01.함수개념.py
+++ myvenv/Chapter06/01.함수개념.py
@@ -9,3 +9,12 @@ print("현재 프리미엄 서비스 사용기간이 15일 남았습니다.")
 
 print("안녕하세요. 켄치마마님")
 print("현재 프리미엄 서비스 사용기간이 7일 남았습니다.")
+
+# 함수를 사용한 경우
+def printMessage(name, date):
+    print("안녕하세요. ", name, "님")
+    print("현재 프리미엄 서비스 사용기간이 ", date, "일 남았습니다.")
+
+printMessage("켄치", 30)
+printMessage("켄치파파", 15)
+printMessage("켄치마마", 7)
